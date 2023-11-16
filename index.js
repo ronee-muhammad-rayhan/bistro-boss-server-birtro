759b745927f5373efe0b07e0a0e9510c88ae4216
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-16 08:13:45 +0600

    68-4 Load all users on the Dashboard page

--- index.js
+++ index.js
@@ -31,6 +31,10 @@ async function run() {
     const cartCollection = client.db("bistroDb").collection("carts");
 
     // users related api
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
     app.post("/users", async (req, res) => {
       const user = req.body;
       // insert email if user doesnt exists:
