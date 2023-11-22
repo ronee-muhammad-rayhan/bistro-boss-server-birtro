789afbf02f9d9b5e404df70918138818f7ef9f84
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-22 16:08:52 +0600

    69-8 (Recap) Create update Menu item form

--- index.js
+++ index.js
@@ -135,11 +135,20 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/menu/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.findOne(query);
+      res.send(result);
+    });
+
     app.post("/menu", verifyToken, verifyAdmin, async (req, res) => {
       const item = req.body;
       const result = await menuCollection.insertOne(item);
       res.send(result);
     });
+
+    app.patch("/menu/:id", async (req, res) => {});
 
     app.delete("/menu/:id", verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
