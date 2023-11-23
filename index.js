c18dc33d6c95858d45130d15502b8797ebd82310
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-23 18:30:44 +0600

    71-4 Load data and Display stats on the admin home

--- index.js
+++ index.js
@@ -243,7 +243,7 @@ async function run() {
       res.send({ paymentResult, deleteResult });
     });
 
-    app.get("/admin-stats", async (req, res) => {
+    app.get("/admin-stats", verifyToken, verifyAdmin, async (req, res) => {
       const users = await userCollection.estimatedDocumentCount();
       const menuItems = await menuCollection.estimatedDocumentCount();
       const orders = await paymentCollection.estimatedDocumentCount();
@@ -256,7 +256,20 @@ async function run() {
       //   0
       // );
 
-      const result = await paymentCollection.aggregate([{}, {}, {}]);
+      const result = await paymentCollection
+        .aggregate([
+          {
+            $group: {
+              _id: null,
+              totalRevenue: {
+                $sum: "$price",
+              },
+            },
+          },
+        ])
+        .toArray();
+
+      const revenue = result.length > 0 ? result[0].totalRevenue : 0;
 
       res.send({
         users,
